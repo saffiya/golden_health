7092018665e68fa4541da6ffc4f4bbc6aaa7c832
saffiya <saffiyax@icloud.com>
2020-12-31 00:53:48 +0000

    added add to bag functionality

--- products/views.py
+++ products/views.py
@@ -1,30 +1,28 @@
+
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
-from .models import Product, Category
 
+from .models import Product, Category
 
 # Create your views here.
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
-    """return all the products from database using the following"""
+
     products = Product.objects.all()
     query = None
     categories = None
     sort = None
     direction = None
 
-    if 'sort' in request.GET:
-            #if the reuest.get parameters do contain sort:
-            #set a variable called sortkey equal to request.get.sort
+    if request.GET:
+        if 'sort' in request.GET:
             sortkey = request.GET['sort']
-            #then set sort from none to sortkey
             sort = sortkey
             if sortkey == 'name':
                 sortkey = 'lower_name'
-                #The actual annotation
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
                 sortkey = 'category__name'
@@ -33,23 +31,21 @@ def all_products(request):
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
-    if 'category' in request.GET:
+            
+        if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
-            # to display to the user which categories they currently have selected
-
-    if request.GET:
 
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-
+            
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
+
     current_sorting = f'{sort}_{direction}'
 
     context = {
