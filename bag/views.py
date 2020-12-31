68b45f3ce8b1300ad2176a4acfa8892120a393eb
saffiya <saffiyax@icloud.com>
2020-12-31 11:49:09 +0000

    Added toasts

--- bag/views.py
+++ bag/views.py
@@ -1,17 +1,19 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
-
 
 def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
 
-
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -25,19 +27,18 @@ def add_to_bag(request, item_id):
                 bag[item_id]['items_by_size'][size] += quantity
             else:
                 bag[item_id]['items_by_size'][size] = quantity
-                messages.success(request, f'Added size {size.upper()} {product.name} to your bag')
-
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
-
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')    
 
-        request.session['bag'] = bag
-        return redirect(redirect_url)
+    request.session['bag'] = bag
+    return redirect(redirect_url)
+    
 
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
@@ -50,7 +51,7 @@ def adjust_bag(request, item_id):
 
     if size:
         if quantity > 0:
-            bag[item_id] = quantity
+            bag[item_id]['items_by_size'][size] = quantity
         else:
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
@@ -63,6 +64,7 @@ def adjust_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
+
 
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
