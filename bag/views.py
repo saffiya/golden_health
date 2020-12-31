39be45ac3c903d4a35606d9a26ad17e378ae1e48
saffiya <saffiyax@icloud.com>
2020-12-31 10:23:59 +0000

    Finished product size logic

--- bag/views.py
+++ bag/views.py
@@ -14,15 +14,30 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-    else:
-        bag[item_id] = quantity
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+                messages.success(request, f'Added size {size.upper()} {product.name} to your bag')
 
-    request.session['bag'] = bag
-    return redirect(redirect_url)
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
+
+    else:
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
+
+        request.session['bag'] = bag
+        return redirect(redirect_url)
 
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
