a32723c5515b845ea45c498752f2a2d786e24c33
saffiya <saffiyax@icloud.com>
2021-01-28 00:02:03 +0000

    Removed secret key and set debug

--- golden_health/settings.py
+++ golden_health/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a@8^6o6g7aihrg@pts0++(=md0mk$31=pu=ioz0xcw6st&j$ut'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['golden-health.herokuapp.com', 'localhost']
 
